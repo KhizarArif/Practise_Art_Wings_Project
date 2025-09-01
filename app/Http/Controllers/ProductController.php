1b5df965f153be3c2e8e2234d8a57eb86375403f
KhizarArif <khizararif201@gmail.com>
2025-09-01 16:12:23 +0500

    Add Backend Functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,32 +21,32 @@ class ProductController extends Controller
     public function store(Request $request){
         return $this->productServices->store($request);
     }
-    public function edit(Request $request){
-        return $this->productServices->edit($request);
-    }
-    public function updateProductImage(Request $request){
-        return $this->productServices->updateProductImage($request);
-    }
-    public function deleteProductImage(Request $request){
-        return $this->productServices->deleteProductImage($request);
-    }
-    public function destroy($id){
-        return $this->productServices->destroy($id);
-    }
-    public function featuredProduct(){
-        return $this->productServices->featuredProduct();
-    }
-    public function createFeaturedProduct(){
-        return $this->productServices->createFeaturedProduct();
-    }
-    public function storeFeaturedProduct(Request $request){
-        return $this->productServices->storeFeaturedProduct($request);
-    }
-    public function editFeaturedProduct(Request $request){
-        return $this->productServices->editFeaturedProduct($request);
-    }
-    public function destroyFeaturedProduct($id){
-        return $this->productServices->destroyFeaturedProduct($id);
-    }
+    // public function edit(Request $request){
+    //     return $this->productServices->edit($request);
+    // }
+    // public function updateProductImage(Request $request){
+    //     return $this->productServices->updateProductImage($request);
+    // }
+    // public function deleteProductImage(Request $request){
+    //     return $this->productServices->deleteProductImage($request);
+    // }
+    // public function destroy($id){
+    //     return $this->productServices->destroy($id);
+    // }
+    // public function featuredProduct(){
+    //     return $this->productServices->featuredProduct();
+    // }
+    // public function createFeaturedProduct(){
+    //     return $this->productServices->createFeaturedProduct();
+    // }
+    // public function storeFeaturedProduct(Request $request){
+    //     return $this->productServices->storeFeaturedProduct($request);
+    // }
+    // public function editFeaturedProduct(Request $request){
+    //     return $this->productServices->editFeaturedProduct($request);
+    // }
+    // public function destroyFeaturedProduct($id){
+    //     return $this->productServices->destroyFeaturedProduct($id);
+    // }
 }
     
